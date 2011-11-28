47a5b243f69c9babcec87163e7478f9564547ff7
Martin Maechler <maechler@r-project.org>
2011-11-28 12:01:59 +0000

    version of 2011.11.28.12.01.59 (now is 15 Oct 2020).

--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,2 @@
 ## no S4 methodology here; speedup :
 .noGenerics <- TRUE
-
-
-## for R versions < 1.5
-if(paste(R.version$major, R.version$minor, sep=".") < 1.5) {
-    ## cheap substitutes :
-    rowMeans <- function(x) apply(x, 1, mean) ## used in many places
-    colMeans <- function(x) apply(x, 2, mean)
-    rowSums  <- function(x) apply(x, 1, sum)
-    colSums  <- function(x) apply(x, 2, sum)
-}
